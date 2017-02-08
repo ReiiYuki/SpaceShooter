145c007f22fe186cdc486a7c53ff571ecaf66aca
ReiiYuki <iguru.shou.5410070@gmail.com>
2017-02-08 17:04:33 +0700

    Player Control Script

---
+++ Assets/SpaceshipController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpaceShipControl();
+    }
+
+    void SpaceShipControl()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * 5f);
+        transform.Translate(Input.GetAxis("Vertical") * Vector3.up * Time.deltaTime * 5f);
+    }
+}
