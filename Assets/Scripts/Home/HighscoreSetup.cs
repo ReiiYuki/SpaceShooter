da445269d0a895b49a694a4198ce1675255eee5d
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-02-21 10:50:05 +0700

    Updatew Highscore

---
+++ Assets/Scripts/Home/HighscoreSetup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = "Highscore : "+( PlayerPrefs.GetInt("highscore") > 0 ? PlayerPrefs.GetInt("highscore") +"" : "0");
+    }
+
+}
